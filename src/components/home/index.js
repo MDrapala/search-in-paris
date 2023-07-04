261a0d3c61c1d63b595231c322770a36789b41ea
MDrapala <drapalamathieu95@gmail.com>
2023-07-04 09:38:32 +0200

    Init

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import { Component } from "react";
+import axios from "axios";
 
-import SearchBar from '../searchBar/index';
-import LoadingSpinner from '../loading/index';
+import SearchBar from "../searchBar/index";
+import LoadingSpinner from "../loading/index";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: '',
+      search: "",
       result: [],
       isLoading: false,
     };
@@ -21,11 +21,14 @@ export default class Home extends Component {
     const { search } = this.state;
 
     this.setState({ isLoading: true }, () => {
-      axios.get(`https://opendata.paris.fr/api/datasets/1.0/search/?q=${search}`)
+      axios
+        .get(`https://opendata.paris.fr/api/datasets/1.0/search/?q=${search}`)
         .then((res) => {
           const resultat = res.data;
           if (resultat.nhits === 0) {
-            document.getElementById('error').innerHTML = `There is nothing for "${search}"`;
+            document.getElementById(
+              "error"
+            ).innerHTML = `There is nothing for "${search}"`;
           } else {
             this.setState({ isLoading: false, result: [resultat] });
           }
@@ -44,9 +47,26 @@ export default class Home extends Component {
     const { result, search, isLoading } = this.state;
     return (
       <div>
-        <SearchBar value={search} onChange={this.handleOnChange} onSubmit={this.handleSubmit} />
+        <SearchBar
+          value={search}
+          onChange={this.handleOnChange}
+          onSubmit={this.handleSubmit}
+        />
         <div className="container" id="error" />
-        {isLoading ? <LoadingSpinner /> : result.map((label, i) => <li className="container" key={label.datasets[i].datasetid}><div className="color" dangerouslySetInnerHTML={{ __html: label.datasets[i].metas.description }} /></li>)}
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          result.map((label, i) => (
+            <li className="container" key={label.datasets[i].datasetid}>
+              <div
+                className="color"
+                dangerouslySetInnerHTML={{
+                  __html: label.datasets[i].metas.description,
+                }}
+              />
+            </li>
+          ))
+        )}
       </div>
     );
   }
